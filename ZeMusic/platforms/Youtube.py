c75c5901f53bfbc43a43a14d0084e21cc050f892
botnwaf <ladlrtu@catway.org>
2024-08-09 16:37:25 +0300

    Update Youtube.py

--- ZeMusic/platforms/Youtube.py
+++ ZeMusic/platforms/Youtube.py
@@ -243,12 +243,10 @@ class YouTubeAPI:
 
         def audio_dl():
             ydl_optssx = {
-                "format": "bestaudio/best",
                 "outtmpl": "downloads/%(id)s.%(ext)s",
-                "geo_bypass": True,
-                "nocheckcertificate": True,
                 "quiet": True,
                 "no_warnings": True,
+                "proxy": "200.174.198.86:8888"
             }
             x = yt_dlp.YoutubeDL(ydl_optssx)
             info = x.extract_info(link, False)
